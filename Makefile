8622799c8df434351773b9bb3944b0c6bef2baa3
Alexandra Lepercq <alepercq@z1r1p5.le-101.fr>
2018-12-20 16:27:57 +0100

    reorganize Makefile & libft.h

--- Makefile
+++ Makefile
@@ -39,87 +39,39 @@ INCLUDE		=	libft.h
 #INCLUDES	=	$(addprefix $(PATH_INC), $(wildcard *.h))
 INCLUDES	=	$(addprefix $(PATH_INC), $(INCLUDE))
 
-SRC			=	ft_atoi.c \
-				ft_bzero.c \
-				ft_isalnum.c \
-				ft_isalpha.c \
-				ft_isascii.c \
-				ft_isdigit.c \
-				ft_isprint.c \
-				ft_itoa.c \
-				ft_lstadd.c \
-				ft_lstdel.c \
-				ft_lstdelone.c \
-				ft_lstmap.c \
-				ft_lstnew.c \
-				ft_lstiter.c \
-				ft_memalloc.c \
-				ft_memccpy.c \
-				ft_memchr.c \
-				ft_memcmp.c \
-				ft_memcpy.c \
-				ft_memdel.c \
-				ft_memmove.c \
-				ft_memset.c \
-				ft_putchar.c \
-				ft_putchar_fd.c \
-				ft_putendl.c \
-				ft_putendl_fd.c \
-				ft_putnbr.c \
-				ft_putnbr_fd.c \
-				ft_putstr.c \
-				ft_putstr_fd.c \
-				ft_strcat.c \
-				ft_strchr.c \
-				ft_strclr.c \
-				ft_strcmp.c \
-				ft_strcpy.c \
-				ft_strdel.c \
-				ft_strdup.c \
-				ft_strequ.c \
-				ft_striter.c \
-				ft_striteri.c \
-				ft_strjoin.c \
-				ft_strlcat.c \
-				ft_strlen.c \
-				ft_strmap.c \
-				ft_strmapi.c \
-				ft_strncat.c \
-				ft_strncmp.c \
-				ft_strncpy.c \
-				ft_strnequ.c \
-				ft_strnew.c \
-				ft_strnstr.c \
-				ft_strrchr.c \
-				ft_strstr.c \
-				ft_strsplit.c \
-				ft_strsub.c \
-				ft_strtrim.c \
-				ft_toupper.c \
-				ft_tolower.c \
-				ft_error.c \
-				ft_int_abs.c \
-				ft_int_find_minmax.c \
-				ft_int_max.c \
-				ft_int_min.c \
-				ft_int_power.c \
-				ft_int_sqrt.c \
+SRC			=	ft_atoi.c ft_int_abs.c \
+				ft_int_find_minmax.c ft_int_max.c ft_int_min.c ft_int_triple_min.c\
+				ft_int_power.c ft_int_sqrt.c \
 				ft_int_swap.c \
-				ft_int_triple_min.c \
-				ft_isblanc.c \
-				ft_isprime.c \
-				ft_lstrev.c \
-				ft_lstlast.c \
-				ft_lstsize.c \
-				ft_memrealloc.c \
-				ft_putuchar_fd.c \
-				ft_putustr_fd.c \
-				ft_strrev.c \
-				ft_strline.c \
-				ft_strswap.c \
-				ft_strword.c \
+				ft_isalnum.c ft_isalpha.c ft_isascii.c ft_isblanc.c \
+				ft_isdigit.c ft_isprime.c ft_isprint.c \
+				ft_lstadd.c ft_lstdel.c ft_lstdelone.c ft_lstiter.c ft_lstlast.c \
+				ft_lstmap.c ft_lstnew.c ft_lstrev.c ft_lstsize.c \
+				ft_bzero.c \
+				ft_memalloc.c ft_memrealloc.c
+				ft_memccpy.c ft_memcpy.c \
+				ft_memchr.c ft_memcmp.c \
+				ft_memdel.c ft_memmove.c \
+				ft_memset.c \
+				ft_putchar.c ft_putchar_fd.c ft_putuchar_fd.c \
+				ft_putendl.c ft_putendl_fd.c \
+				ft_putnbr.c ft_putnbr_fd.c \
+				ft_putstr.c ft_putstr_fd.c ft_putustr_fd.c \
+				ft_itoa.c \
+				ft_strcat.c ft_strlcat.c ft_strncat.c ft_strchr.c ft_strclr.c
+				ft_strcmp.c ft_strncmp.c \
+				ft_strcpy.c ft_strncpy.c \
+				ft_strdel.c ft_strdup.c ft_strequ.c ft_strnequ.c ft_striter.c ft_striteri.c \
+				ft_strjoin.c  ft_strlen.c ft_strline.c \
+				ft_strmap.c ft_strmapi.c  ft_strnew.c \
+				ft_strnstr.c ft_strrchr.c ft_strstr.c \
+				ft_strsplit.c ft_strsub.c ft_strswap.c \
+				ft_strword.c ft_strtrim.c \
+				ft_toupper.c ft_tolower.c \
+				get_next_line.c \
+				ft_error.c \
 				ft_time_wait.c \
-				get_next_line.c
+				
 SRCS		=	$(addprefix $(PATH_SRC), $(SRC))
 
 OBJ			=	$(SRC:.c=.o)
--- includes/libft.h
+++ includes/libft.h
@@ -23,6 +23,9 @@
 
 # define BUFF_SIZE 4096
 
+/*
+** structure definition
+*/
 typedef	struct	s_list
 {
 	void			*content;
@@ -30,20 +33,47 @@ typedef	struct	s_list
 	struct s_list	*next;
 }				t_list;
 
+/*
+** function integer
+*/
 int				ft_atoi(char const *str);
-void			ft_bzero(void *v, size_t n);
+int				ft_int_abs(int n);
+void			ft_int_find_minmax(int c_sum, int c_min, int c_max);
+int				ft_int_max(int *vref, int vsum);
+int				ft_int_min(int *vref, int vsum);
+int				ft_int_power(int nb, int pwr);
+int				ft_int_sqrt(int nb);
+void			ft_int_swap(int *a, int *b);
+int				ft_int_triple_min(int vala, int valb, int valc);
+
+/*
+** function is
+*/
 int				ft_isalnum(int i);
 int				ft_isalpha(int i);
 int				ft_isascii(int i);
+int				ft_isblanc(char c);
 int				ft_isdigit(int i);
+int				ft_isprime(int p);
 int				ft_isprint(int i);
-char			*ft_itoa(int n);
+
+/*
+** function linked list
+*/
 void			ft_lstadd(t_list **alst, t_list *blst);
 void			ft_lstdel(t_list **alst, void (*del)(void *, size_t));
 void			ft_lstdelone(t_list **alst, void (*del)(void*, size_t));
+void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list			*ft_lstlast(t_list *alist);
 t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 t_list			*ft_lstnew(void const *content, size_t content_size);
-void			ft_lstiter(t_list *lst, void (*f)(t_list *elem));
+t_list			*ft_lstrev(t_list *blst);
+int				ft_lstsize(t_list *alist);
+
+/*
+** function memory
+*/
+void			ft_bzero(void *v, size_t n);
 void			*ft_memalloc(size_t size);
 void			*ft_memccpy(void *dst, void const *src, int c, size_t n);
 void			*ft_memchr(void const *v, int c, size_t n);
@@ -51,15 +81,27 @@ int				ft_memcmp(void const *v1, void const *v2, size_t n);
 void			*ft_memcpy(void *dst, void const *src, size_t n);
 void			ft_memdel(void **ap);
 void			*ft_memmove(void *dst, void const *src, size_t len);
+void			*ft_memrealloc(void *v, size_t size);
 void			*ft_memset(void *v, int c, size_t len);
+
+/*
+** function screen display
+*/
 void			ft_putchar(char c);
 void			ft_putchar_fd(char c, int fd);
+void			ft_putuchar_fd(int fd, wchar_t c);
 void			ft_putendl(char const *str);
 void			ft_putendl_fd(char const *s, int fd);
 void			ft_putnbr(int n);
 void			ft_putnbr_fd(int n, int fd);
 void			ft_putstr(char const *str);
 void			ft_putstr_fd(char const *str, int fd);
+void			ft_putustr_fd(int fd, wchar_t *str);
+
+/*
+** function string
+*/
+char			*ft_itoa(int n);
 char			*ft_strcat(char *str1, char const *str2);
 char			*ft_strchr(char const *str, int c);
 void			ft_strclr(char *str);
@@ -73,6 +115,7 @@ void			ft_striteri(char *str, void (*f)(unsigned int, char *));
 char			*ft_strjoin(char const *s1, char const *s2);
 size_t			ft_strlcat(char *dst, char const *src, size_t size);
 size_t			ft_strlen(char const *str);
+int				ft_strline(char *str);
 char			*ft_strmap(char const *str, char (*f)(char));
 char			*ft_strmapi(char const *str, char (*f)(unsigned int, char));
 char			*ft_strncat(char *str1, char const *str2, size_t len);
@@ -81,37 +124,30 @@ char			*ft_strncpy(char *dst, char const *src, size_t len);
 int				ft_strnequ(char const *s1, char const *s2, size_t n);
 char			*ft_strnew(size_t size);
 char			*ft_strnstr(char const *str, char const *to_find, size_t len);
+char			*ft_strrev(char *str);
 char			*ft_strrchr(char const *str, int c);
 char			**ft_strsplit(char const *str, char c);
 char			*ft_strstr(char const *str, char const *to_find);
 char			*ft_strsub(char const *s, unsigned int start, size_t len);
+void			ft_strswap(char *str1, char *str2);
 char			*ft_strtrim(char const *str);
+int				ft_strword(char const *s, char c);
 int				ft_toupper(int i);
 int				ft_tolower(int i);
 
+/*
+** function file
+*/
 int				get_next_line(const int fd, char **line);
 
-void			ft_error(int error, char *filename);
-int				ft_int_abs(int n);
-void			ft_int_find_minmax(int c_sum, int c_min, int c_max);
-int				ft_int_max(int *vref, int vsum);
-int				ft_int_min(int *vref, int vsum);
-int				ft_int_power(int nb, int pwr);
-int				ft_int_sqrt(int nb);
-void			ft_int_swap(int *a, int *b);
-int				ft_int_triple_min(int vala, int valb, int valc);
-int				ft_isblanc(char c);
-int				ft_isprime(int p);
-t_list			*ft_lstlast(t_list *alist);
-t_list			*ft_lstrev(t_list *blst);
-int				ft_lstsize(t_list *alist);
-void			ft_putuchar_fd(int fd, wchar_t c);
-void			ft_putustr_fd(int fd, wchar_t *str);
-void			*ft_memrealloc(void *v, size_t size);
-int				ft_strline(char *str);
-char			*ft_strrev(char *str);
-void			ft_strswap(char *str1, char *str2);
-int				ft_strword(char const *s, char c);
+/*
+** function time
+*/
 void			ft_time_wait(unsigned long int time_delay);
+
+/*
+** function miscellenaous
+*/
+void			ft_error(int error, char *filename);
 
 #endif
